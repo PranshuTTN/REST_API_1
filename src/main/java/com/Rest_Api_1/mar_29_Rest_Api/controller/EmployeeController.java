76e7b92e241cf3e7c5c8f993dac81db3f220f2ce
PranshuTTN <pranshu.saluja@tothenew.com>
2023-04-03 16:43:06 +0530

    Q3 added

--- src/main/java/com/Rest_Api_1/mar_29_Rest_Api/controller/EmployeeController.java
+++ src/main/java/com/Rest_Api_1/mar_29_Rest_Api/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.Rest_Api_1.mar_29_Rest_Api.controller;
 
+import com.Rest_Api_1.mar_29_Rest_Api.entities.Employee;
 import com.Rest_Api_1.mar_29_Rest_Api.service.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -12,5 +15,9 @@ public class EmployeeController {
     @GetMapping("/welcome")
     public String greetings(){
         return "Welcome to spring boot";
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeDao.getAllEmployees();
     }
 }
